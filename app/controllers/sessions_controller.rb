7e29c44c913103d9f7094148e884fa8b74094021
ezequias2d <ezequiasmoises@gmail.com>
2023-01-17 19:04:23 -0300

    Add sign in view

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,13 @@
+class SessionsController < ApplicationController
+  layout "session"
+
+  def new
+  end
+  
+  def create
+  end
+
+  def destroy
+  end
+end
+
--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,12 @@ Rails.application.routes.draw do
   resources :articles
   post '/articles/new' => 'articles#create'
 
-  get 'signup', to: 'users#new'
+  get 'register', to: 'users#new'
   resources :users, expect: [:new]
+  
+  get 'signin', to: 'sessions#new'
+  post 'signin', to: 'sessions#create'
+  delete 'signout', to: 'sessions#delete'
 
   root 'welcome#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
