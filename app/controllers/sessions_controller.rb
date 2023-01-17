cdbcbaebe6ccf0ddec7787969cf23fc73b328491
ezequias2d <ezequiasmoises@gmail.com>
2023-01-17 19:41:02 -0300

    Add sign in and sign out actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,9 +5,20 @@ class SessionsController < ApplicationController
   end
   
   def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      redirect_to user
+    else
+      flash.now[:alert] = "You have entered an invalid email or password"
+      render 'new'
+    end
   end
 
-  def destroy
+  def delete
+    session[:user_id] = nil
+    flash[:notice] = "You have been signed out successfully"
+    redirect_to root_path
   end
 end
 
