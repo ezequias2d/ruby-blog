d6a6378b293d5bed618edd0e1dd9ba65dcaa44e7
ezequias2d <ezequiasmoises@gmail.com>
2023-01-18 22:16:28 -0300

    Add ArticleCategory(many to many) model

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,6 @@ class Article < ApplicationRecord
   validates :title, presence: true, length: { minimum: 5, maximum: 100 }
   validates :text, presence: true, length: { minimum: 10, maximum: 600 }
   belongs_to :user
+  has_many :article_categories
+  has_many :categories, through: :article_categories
 end
